f0a5e019766b5a8f47fc4ad10b9cdc7906290c2b
Roshan <roshanshrestha0921@gmail.com>
2024-03-13 21:53:23 +0545

    Add project files.

---
+++ CustomLogin/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CustomLogin.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomLogin.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
