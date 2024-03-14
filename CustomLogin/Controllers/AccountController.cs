08d46385baf062f22eac0e96bd70eef29492c77f
Roshan <roshanshrestha0921@gmail.com>
2024-03-14 08:03:25 +0545

    c1

---
+++ CustomLogin/Controllers/AccountController.cs
@@ -0,0 +1,76 @@
+﻿using CustomLogin.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using CustomLogin.Models;
+
+namespace CustomLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<AppUser> signInManager;
+        private readonly UserManager<AppUser> userManager;
+
+
+        public AccountController(SignInManager<AppUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVm model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Login
+                var result= await signInManager.PasswordSignInAsync(model.Username!, model.Password!, model.RememberMe,false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid Login attemp");
+                return View(model);
+            }
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser ()
+                {
+                    Name = model.Name,
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Address = model.Address
+                };
+                 var result = await userManager.CreateAsync(user,model.Password!);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View();
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();           
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
